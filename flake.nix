476bd3cc66ad5031aff5c33105ab8c29b08395aa
anntnzrb <anntnzrb@proton.me>
2025-07-10 08:34:01 -0500

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+    flake-parts.url = "github:hercules-ci/flake-parts/main";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.typst
+              pkgs.typstyle
+              pkgs.shfmt
+            ];
+          };
+        };
+    };
+}
